21ab496e2129ed4ecd56f9fef8e9d5e126506801
jnicholson-wcgs <54621945+jnicholson-wcgs@users.noreply.github.com>
2021-04-30 10:03:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,52 +3,92 @@
 namespace L6Hash
 {
 
-    /*
-     * Tasks:
-     * 1) Complete the implementation of the Node methods
-     * 2) Print out the tree using the different tree traversal metods
-     * 3) Test findNote() and deleteNode()
-     *
-     *
-     */
-    class Node
-    {
-        // Attributes
-        private  Node left;
-        private  Node right;
-        private string item;
+    class HashItem {
+        public string _value;
+        public string _name;
+        public bool _deleted;
 
-        //Methods
-        public Node(string item) { }
-        public void addNode(string item) {}
-        public Boolean findNode(string item) { return true; }
-        public Boolean deleteNote(string item) { return true; }
-        void printTree() { }
+        // HashItem constructor - initialise each attribute
+        public HashItem () {
+            _value = null;
+            _name = null;
+            _deleted = false;
+  
+        }
+  }
+  class HashTable {
+    HashItem [] _hashTable;
+    int hashSize;   // Size of hash table
+
+    public HashTable (int size = 100) {
+      hashSize = size;
+
+      // Allocate the hash table from the heap. 
+      // The table is an array of HashItem object references
+      _hashTable = new HashItem [size];   // the hash table
+
+      // For each reference in the hash table, link it to a newly
+      // constructed HashItem
+      for (int i = 0; i < _hashTable.Length; i++)
+        _hashTable[i] = new HashItem();
     }
+
+    public int HashFunction (string name) {
+      
+      // You need to implement a proper HashFucntion here!
+      Console.WriteLine ("HashFunction() returns 0");
+      return 0;
+    }
+
+    // Add item to hash table
+    public void Add (string name, string value) {
+      
+      // Get the start index into the hash table
+      int i = HashFunction (name);
+
+      // Debug for the current index
+      // TODO: This code does not deal with collision!
+        
+      //if (_hashTable[i]._name == null)
+        //Console.WriteLine ("Empty hash");
+      //else
+        //Console.WriteLine ("Full hash");
+
+      // Enter the name, value pair in the hash table
+      _hashTable[i]._name = name;
+      _hashTable[i]._value = value;
+      Console.WriteLine ("Added ({0}, {1}) in slot {2}", name, value, i);
+    }
+    
+    // Print out debug and load factor information about the hash table
+    public void PrintInfo() {
+        Console.WriteLine ("TODO: Implement PrintInfo()");
+    }
+  }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Node root = null;
+            // Create the hash table
+            HashTable h = new HashTable (53);
 
             string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
+            // Add the data into the hash table
+            for (int i = 0; i < months.Length; i++)
+                h.Add (months[i], (i+1).ToString());
 
-            // process all the nodes on the array
-            //
-            foreach (var mon in months)
-            {
-                if (root == null)
-                    root = new Node(mon);
-                else
-                    root.addNode(mon);
-            }
+            // Print the stats in the hash table
+    
+            h.PrintInfo();
 
-            // print out the tree using different traversal methods
-            //
+            // Add more data to generate collisions
+    
+            // Loop to find data in the hash table
 
-            // Test the findNote() and deleteNode()
-        }
+            // Extension 1: Delete an item from the hash table
+
+            // Extension 2: Rehash the table
     }
 }
